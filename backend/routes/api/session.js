ad75a14e89f74a2a465bde133add8582a0700323
Nicolas Borromeo <nicoborrom@gmail.com>
2024-10-02 13:21:07 -0700

    user login

---
+++ backend/routes/api/session.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const router = require('express').Router();
+const { Op } = require('sequelize')
+const bcrypt = require('bcryptjs')
+
+const { setTokenCookie, restoreUser} = require('../../utils/auth')
+const { User } = require('../../db/models');
+
+router.post(
+    '/',
+    async (req, res, next) => {
+        const {credential, password} = req.body;
+
+        const user = await User.unscoped().findeOne({
+            where: {
+                [Op.or]: {
+                    username: credential,
+                    email: credential
+                }
+            }
+        });
+
+        if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
+            const err = new Error('Login Failed');
+            err.status = 401;
+            err.title = 'Login Failed';
+            err.errors = { credential: 'The provided credentials were invalid.' };
+            return next(err);
+        }
+
+        const safeUser = {
+            id: user.id,
+            email: user.email,
+            username: user.username,
+        };
+
+        await setTokenCookie(res, safeUser);
+
+        return res.json({
+            user: safeUser
+        });
+    }
+);
+
+
+module.exports = router
