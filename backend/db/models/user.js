ce26f4596f718fbcdaf7d617373d17c9d6499103
Nicolas Borromeo <nicoborrom@gmail.com>
2024-10-02 13:03:21 -0700

    auth

---
+++ backend/db/models/user.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const { Model } = require('sequelize');
+const Validator = require('validator');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+    }
+  }
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [4, 30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
+            throw new Error('Cannot be an email.');
+          }
+        },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [3, 256],
+        isEmail: true,
+      },
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+      validate: {
+        len: [60, 60],
+      },
+    },
+  },
+  {
+    sequelize,
+    modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt'],
+      }
+    }
+  }
+);
+return User;
+};
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
-router.post('/test', function(req, res){
-    res.json({ requestBody: req.body });
-});
+const { restoreUser } = require('../../utils/auth.js');
+
+router.use(restoreUser)
+
+
 
 
 module.exports = router;
