d22d1332d623df407d348fde1ffba835da9a3af7
Nicolas Borromeo <nicoborrom@gmail.com>
2024-10-02 12:15:08 -0700

    error handlers

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
+const { ValidationError } = require('sequelize');
+
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -23,25 +25,61 @@ if (!isProduction) {
     app.use(cors());
 }
 
-  // helmet helps set a variety of headers to better secure your app
+// helmet helps set a variety of headers to better secure your app
 app.use(
-helmet.crossOriginResourcePolicy({
-    policy: "cross-origin"
-})
+    helmet.crossOriginResourcePolicy({
+        policy: "cross-origin"
+    })
 );
 
 // Set the _csrf token and create req.csrfToken method
 app.use(
-csurf({
-    cookie: {
-    secure: isProduction,
-    sameSite: isProduction && "Lax",
-    httpOnly: true
-    }
-})
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
 );
 
 const routes = require('./routes');
 app.use(routes);
+
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found." };
+    err.status = 404;
+    next(err);
+});
+
+
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+        let errors = {};
+        for (let error of err.errors) {
+            errors[error.path] = error.message;
+        }
+        err.title = 'Validation error';
+        err.errors = errors;
+    }
+    next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
 
 module.exports = app;
