563d196e99537af607e0e2abbde41b35b18eb652
Nicolas Borromeo <nicoborrom@gmail.com>
2024-10-02 13:22:43 -0700

    logout

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -43,4 +43,13 @@ router.post(
 );
 
 
+router.delete(
+    '/',
+    (_req, res)=> {
+        res.clearCookie('token');
+        return res.json({message: 'success'});
+    }
+);
+
+
 module.exports = router
