b91053bef0448c339bedb727d1fed11b0554126a
Nicolas Borromeo <nicoborrom@gmail.com>
2024-10-04 13:50:23 -0700

    display info routes

---
+++ backend/routes/api/displayinfo.js
@@ -0,0 +1,42 @@
+const express = require('express');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const { requireAuth } = require('../../utils/auth');
+const { User, UserDisplayInfo, ExternalLink } = require('../../db/models');
+
+const router = express.Router();
+
+router.post('/current',
+    requireAuth,
+    async (req, res) => {
+        let user = req.user;
+        let userHasInfo = await UserDisplayInfo.findOne({
+            where: {userId : user.id}
+        });
+        if(userHasInfo) return res.status(400).json({error: 'You already have a display info. If you want to update it use the Update button'});
+
+        let usersDisplayInfo = await UserDisplayInfo.create({userId: user.id, ...req.body})
+
+        return res.json({message:'Succesfully stored your display info', displayInfo: usersDisplayInfo});
+    }
+)
+
+router.put('/current',
+    requireAuth,
+    async (req, res) => {
+        let user = req.user;
+        let userInfo = await UserDisplayInfo.findOne({
+            where: {userId : user.id}
+        });
+        if(!userInfo) return res.status(400).json({error: 'You do not have a display info to update. Set your default info first'});
+
+        let usersDisplayInfo = await userInfo.update({userId: user.id, ...req.body})
+
+        return res.json({message:'Succesfully updated your display info', displayInfo: usersDisplayInfo});
+    }
+)
+
+
+module.exports = router
