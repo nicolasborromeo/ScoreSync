6a19d0a89ab748710887e2173c783bbfe939362a
Nicolas Borromeo <nicoborrom@gmail.com>
2024-10-14 00:09:30 -0700

    more process in cardsDetails

--- frontend/src/components/CardDetails/CardDetails.jsx
+++ frontend/src/components/CardDetails/CardDetails.jsx
@@ -2,32 +2,34 @@ import { useSelector, useDispatch } from "react-redux"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { thunkGetCurrentCard } from "../../store/cards"
+import './CardDetails.css'
 
-
-export default function CardDetails(){
+export default function CardDetails() {
     const dispatch = useDispatch()
-    let {cardId} = useParams()
+    let { cardId } = useParams()
     const [card, setCard] = useState({})
     const [displayInfo, setDisplayInfo] = useState({})
     const [externalLinks, setExternalLinks] = useState({})
+    const [loaded, setLoaded] = useState(false)
 
-    useEffect(()=> {
-        dispatch(thunkGetCurrentCard(cardId)).then((data)=> setCard(data))
+    useEffect(() => {
+        dispatch(thunkGetCurrentCard(cardId)).then((data) => setCard(data))
     }, [dispatch, setCard, cardId])
 
-    useEffect(()=>{
-        if(card.User){
+    useEffect(() => {
+        if (card.User) {
             setDisplayInfo(card.User.UserDisplayInfo)
             setExternalLinks(card.User.ExternalLinks)
-        } else{
+            setLoaded(true)
+        } else {
             console.log('NO USER', card)
         }
-    }, [dispatch, card, setDisplayInfo, setExternalLinks])
+    }, [dispatch, card, setDisplayInfo, setExternalLinks, loaded])
 
-    return (
+    if(loaded) return (
         <div id="card-details-container">
             <section id="card-banner">
-                <img src={card.Banner.url}/>
+                <img src={card.Banner.url} />
             </section>
             <section id="card-user-info">
                 <div>
@@ -49,8 +51,13 @@ export default function CardDetails(){
                 <div id="card-download-option"></div>
             </section>
             <section id="card-bio">
-                <div id="card-headshot-container"><img src={card.Headshot.url}/></div>
-                <p id="card-bio-text">{displayInfo.bio}</p>
+                <div id="card-headshot-container"><img src={card.Headshot.url} /></div>
+                <div id="card-bio-text">
+                    {displayInfo?.bio.split('\n').map(paragraph => (
+                        <p>{paragraph}</p>
+                    ))}
+
+                </div>
                 <div id="card-contact-info"></div>
             </section>
 
