9b69e01ac0b16eeda38a6dd08eb76e43c6729844
Nicolas Borromeo <nicoborrom@gmail.com>
2024-10-17 14:40:03 -0700

    schema update on migrations

--- backend/db/migrations/20241014203534-create-card-track.js
+++ backend/db/migrations/20241014203534-create-card-track.js
@@ -1,4 +1,11 @@
 'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA
+}
+
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -40,9 +47,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
       },
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('CardTracks');
+    options.tableName = 'CardTracks';
+    await queryInterface.dropTable(options);
   }
 };
